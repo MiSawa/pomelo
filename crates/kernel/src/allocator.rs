11cd270cd71dd9090d4d1d00204244e07d4f6e44
mi_sawa <mi.sawa.1216+git@gmail.com>
2022-01-07 04:49:57 +0900

    Maybe this works!

--- crates/kernel/src/allocator.rs
+++ crates/kernel/src/allocator.rs
@@ -1,3 +1,5 @@
+use core::alloc::GlobalAlloc;
+
 use linked_list_allocator::LockedHeap;
 use pomelo_common::memory_mapping::MemoryMapping;
 use x86_64::structures::paging::page::PageSize;
@@ -5,7 +7,7 @@ use x86_64::structures::paging::page::PageSize;
 use crate::memory_manager::{self, FrameSize};
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: UninterruptedAlloc = UninterruptedAlloc::empty();
 
 const MEMORY_LIMIT: usize = 768 * 1024 * 1024;
 
@@ -16,5 +18,27 @@ pub fn initialize(memory_mapping: &MemoryMapping) {
         .expect("Unable to allocate memory frame......");
     let start_address = allocated.start.start_address().as_u64() as usize;
     let size = allocated.end.start_address().as_u64() as usize - start_address;
-    unsafe { ALLOCATOR.lock().init(start_address, size) }
+    ALLOCATOR.init(start_address, size);
 }
+
+struct UninterruptedAlloc(LockedHeap);
+impl UninterruptedAlloc {
+    const fn empty() -> Self {
+        Self(LockedHeap::empty())
+    }
+    fn init(&self, start: usize, size: usize) {
+        x86_64::instructions::interrupts::without_interrupts(|| unsafe {
+            self.0.lock().init(start, size);
+        });
+    }
+}
+unsafe impl GlobalAlloc for UninterruptedAlloc {
+    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+        x86_64::instructions::interrupts::without_interrupts(|| self.0.alloc(layout))
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+        x86_64::instructions::interrupts::without_interrupts(|| self.0.dealloc(ptr, layout))
+    }
+}
+
